002cbd69876b20754584df390352600f4510e0d5
romiokaa <116096900+romiokaa@users.noreply.github.com>
2023-10-20 11:57:24 +0200

    Add files via upload

---
+++ craps/craps/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  craps
+//
+//  Created by Romaissaa on 16/10/2023.
+//
+
+//#include <stdio.h>
+//#include "craps.h"
+//
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    game();
+//    return 0;
+//}
